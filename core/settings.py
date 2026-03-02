2e795c7b8850d5c7dfde7ea96e539ef427cb6c08
asibgon962 <asibgon962@fp.iesromerovargas.com>
2026-03-02 18:36:10 +0100

    feat: define User and Organization models with migrations

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
+    'users',
+    'catalog',
+    'orders',
+
 ]
 
 MIDDLEWARE = [
@@ -99,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+AUTH_USER_MODEL = 'users.User'
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
