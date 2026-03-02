9e0de7da4c40140a36741a601d44259578db6480
asibgon962 <asibgon962@fp.iesromerovargas.com>
2026-03-02 18:23:05 +0100

    feat: defino User y Organization models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Organization(models.Model):
+    nombre = models.CharField(max_length=100)
+    codigo_grupo = models.CharField(max_length=20, unique=True)
+    es_ilegal = models.BooleanField(default=False) # Para acceso a Dark Web
+
+    def __str__(self):
+        return self.nombre
+
+class User(AbstractUser):
+    # Relación con la organización (Many-to-One)
+    organization = models.ForeignKey(
+        Organization, 
+        on_delete=models.SET_NULL, 
+        null=True, 
+        blank=True,
+        related_name='members'
+    )
