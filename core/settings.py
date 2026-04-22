68859be2c04e7fb9b5b138328d463655f6ee66e0
asibgon962 <asibgon962@users.noreply.github.com>
2026-04-22 11:12:23 +0200

    Solución Bucle de de señales de render

--- core/settings.py
+++ core/settings.py
@@ -177,6 +177,7 @@ SESSION_SAVE_EVERY_REQUEST = False # No renueva sesión en cada petición (rendi
 
 # SR-004 [SECURITY HEADERS] Solo en producción
 if not DEBUG:
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     SECURE_SSL_REDIRECT = True
     SECURE_HSTS_SECONDS = 31536000
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
